35edec9640656be99de0ba9004aeb1d9dde14ce3
nikalinov <nikitakalinovskij@gmail.com>
2023-06-04 22:08:04 +0100

    Created the home/index page and placeholders for the future books/authors pages using templates, URL mappings, and views.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Count the books that start with 'The'
+    books_with_the = Book.objects.filter(title__regex=r'The.*').count()
+    # Count the number of genres
+    num_genres = Genre.objects.count()
+    # Count the number of genres containing 'fiction'
+    fiction_genres = Genre.objects.filter(name__icontains='fiction').count()
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'books_with_the': books_with_the,
+        'num_genres': num_genres,
+        'fiction_genres': fiction_genres,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
