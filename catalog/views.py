2a371e9d94078fd9f489258a4573de7321a07364
nikalinov <nikitakalinovskij@gmail.com>
2023-06-05 22:00:00 +0100

    Added book list and detail pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
 
-    # Generate counts of some of the main objects
-    num_books = Book.objects.all().count()
+    num_books = Book.objects.count()
     num_instances = BookInstance.objects.all().count()
-    # Count the books that start with 'The'
     books_with_the = Book.objects.filter(title__regex=r'The.*').count()
-    # Count the number of genres
     num_genres = Genre.objects.count()
     # Count the number of genres containing 'fiction'
     fiction_genres = Genre.objects.filter(name__icontains='fiction').count()
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
@@ -32,3 +29,10 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    ordering = ['title']
+
+class BookDetailView(generic.DetailView):
+    model = Book
