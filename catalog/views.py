7ac5f77beccd3dc2025811e46e145dc2809075eb
nikalinov <nikitakalinovskij@gmail.com>
2023-06-05 22:56:39 +0100

    Added author list and detail pages

--- catalog/views.py
+++ catalog/views.py
@@ -36,3 +36,11 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 8
+    ordering = ['last_name']
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
