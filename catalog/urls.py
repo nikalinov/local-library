3c256786147c6c5f1f3515a855ca23c0b08a6368
nikalinov <nikitakalinovskij@gmail.com>
2023-06-09 17:42:23 +0200

    Added loaned book renewal form (permitted only for librarians)

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('allborrowed/', views.LoanedBooksListView.as_view(), name='all-borrowed')
+    path('allborrowed/', views.LoanedBooksListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
 
