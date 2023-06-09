819d5fe9bae7159bee65d55502fae3d7f0b840b0
nikalinov <nikitakalinovskij@gmail.com>
2023-06-09 21:47:29 +0200

    Added author create/change/delete functionality on the site

--- catalog/views.py
+++ catalog/views.py
@@ -116,3 +116,23 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', context)
 
+# imports for editing views
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from catalog.models import Author
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'catalog.can_manipulate_authors'
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'catalog.can_manipulate_authors'
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'catalog.can_manipulate_authors'
+    model = Author
+    success_url = reverse_lazy('authors')
