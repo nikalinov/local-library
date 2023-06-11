38a9d5382a02088dced4a7bc37ff72b146041ab5
nikalinov <nikitakalinovskij@gmail.com>
2023-06-11 17:27:55 +0200

    Introduced basic tests for the Author model

--- catalog/models.py
+++ catalog/models.py
@@ -82,10 +82,10 @@ class BookInstance(models.Model):
 
 class Author(models.Model):
     """Model representing an author"""
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
