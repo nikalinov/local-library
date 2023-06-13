414f0cc8caa35db5b7c4c3e46448ffc46c811892
nikalinov <nikitakalinovskij@gmail.com>
2023-06-13 22:15:31 +0200

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with Railway site URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -35,11 +35,10 @@ SECRET_KEY = os.environ.get(
 # Environment variables are strings and not Python types.
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['web-production-cf9c7.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-cf9c7.up.railway.app']
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
